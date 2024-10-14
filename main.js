1a2397df7b625ca0224e70c522870e6ed2c0bcef
sebaszzpinosa@gmail.com <user.emal "sebaszzpinosa@gmail.com>
2024-10-13 23:50:32 -0500

    cambios  visuales  y  de funcionalidad

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
 console.log("hola juego");
+/* variables del juego */
 
-const rock = document.getElementById("rock").addEventListener("click", () => {
-  selectUser(ROCK);
-});
-const paper = document.getElementById("paper");
-const excisor = document.getElementById("excisor");
+const buttons = document.querySelectorAll(".game-button");
 const winLose = document.querySelector(".win");
+const theme = document.getElementById("theme");
+const userOption = document.getElementById("userOption")
+const compOption = document.getElementById("compOption")
+
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    selectUser(index + 1);
+  });
+});
+/*-------------CONSTANTES DEL JUEGO ----------ny */
 const ROCK = 1;
 const PAPER = 2;
 const EXCISOR = 3;
 const win = "ganaste ";
-const lose = "perdiste, intentalo en otra ocacion";
+const lose = "perdiste, intentalo en otra ocación";
 const tie = "empate, no puedes ser mejor?";
 
-paper.addEventListener("click", () => {
-  selectUser(PAPER);
-});
 
-excisor.addEventListener("click", () => {
-  selectUser(EXCISOR);
-});
-
+/* -------------FUNCIONES DEL JUEGO---------  */
 function selectUser(select) {
   console.log(select);
+  changeImage(select);
   const compSelection = selectComp();
   // const resultText=result();
   play(select, compSelection);
 }
 
+/*--TRABJAR EN SET INETEAVAL Y SET TIME OT*/
+setTimeout(() => selectComp(), 5000);
 function selectComp() {
   const compSelection = Math.floor(Math.random() * 3) + 1;
+  // Cambiar la imagen de la computadora después de 2 segundos
+  setTimeout(() => changeImageComp(selectComp), 5000);
+  changeImageComp(compSelection);
   console.log(compSelection);
   return compSelection;
 }
-// function result() {
-//     // winLose.classList.toggle('d-none')
-//     winLose.innerHTML=(tie);
-// }
+
 function play(selectUser, selectComp) {
   if (selectUser === selectComp) {
     console.log("Empate");
@@ -59,3 +64,38 @@ function play(selectUser, selectComp) {
     console.log("Juego nulo");
   }
 }
+
+
+function changeImage(selectUser) {
+  if (selectUser === ROCK) {
+    userOption.src = 'img/roca.png';
+  } else if (selectUser === PAPER) {
+    userOption.src = 'img/papel.png';
+  } else if (selectUser === EXCISOR) {
+    userOption.src = 'img/tijeras.png';
+  }
+}
+
+function changeImageComp(selectComp) {
+  if (selectComp === 1) {
+    compOption.src = 'img/roca.png';
+  } else if (selectComp === 2) {
+    compOption.src = 'img/papel.png';
+  } else if (selectComp === 3) {
+    compOption.src = 'img/tijeras.png';
+  }
+}
+
+
+
+/* FUNCIONES DE ESTILOS */
+theme.addEventListener("change", () => {
+  if (theme.checked) {
+    console.log("activado modo oscuro");
+    document.body.classList.add("dark-theme");
+    document.h2.classList.add("dark-theme")
+  } else {
+    console.log("activado modo claro");
+    document.body.classList.remove("dark-theme");
+  }
+});
